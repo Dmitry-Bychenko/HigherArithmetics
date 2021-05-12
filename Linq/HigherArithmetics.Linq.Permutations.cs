b345b31f7a975e89a9fbbc53fcdf3645f8b6e739
Dmitry Bychenko <Zwerg.CopperNickel@gmail.com>
2021-05-12 22:43:12 +0300

    Permutations

--- Linq/HigherArithmetics.Linq.Permutations.cs
+++ Linq/HigherArithmetics.Linq.Permutations.cs
@@ -17,7 +17,7 @@ namespace HigherArithmetics.Linq {
     #region Public
 
     /// <summary>
-    /// Enumerate permutation starting from items
+    /// Enumerate permutations starting from items
     /// </summary>
     /// <param name="items">initial permutation to use</param>
     /// <param name="comparer">comparer to compare items</param>
@@ -85,11 +85,84 @@ namespace HigherArithmetics.Linq {
     }
 
     /// <summary>
-    /// Enumerate permutation starting from items
+    /// Enumerate permutations starting from items
     /// </summary>
     /// <param name="items">initial permutation to use</param>
-    public static IEnumerable<T[]> Permutations<T>(this IEnumerable<T> items) where T : IComparable<T> =>
-      Permutations(items, null);
+    /// <param name="comparer">comparer to compare items</param>
+    public static IEnumerable<T[]> Permutations<T>(IEnumerable<T> items, IEqualityComparer<T> comparer) {
+      if (items is null)
+        throw new ArgumentNullException(nameof(items));
+
+      comparer ??= EqualityComparer<T>.Default;
+
+      T[] data = items.ToArray();
+      T[] initial = data.ToArray();
+
+      Dictionary<T, int> dict = new Dictionary<T, int>(comparer);
+
+      foreach (T item in data)
+        if (!dict.ContainsKey(item))
+          dict.Add(item, dict.Count);
+
+      if (data.Length <= 0)
+        yield break;
+      if (data.Length == 1) {
+        yield return data;
+
+        yield break;
+      }
+
+      do {
+        yield return data.ToArray();
+
+        int pivotIndex = -1;
+
+        for (int i = data.Length - 2; i >= 0; --i) {
+          if (dict[data[i]] < dict[data[i + 1]]) {
+            pivotIndex = i;
+
+            break;
+          }
+        }
+
+        if (pivotIndex < 0)
+          Array.Sort(data, (a, b) => dict[a].CompareTo(dict[b]));
+        else {
+          int minIndex = -1;
+          T pivot = data[pivotIndex];
+          T min = default;
+
+          for (int i = data.Length - 1; i > pivotIndex; --i) {
+            T item = data[i];
+
+            if (dict[item] > dict[pivot]) {
+              if (minIndex < 0 || dict[item] < dict[min]) {
+                min = item;
+                minIndex = i;
+              }
+            }
+          }
+
+          T h = data[pivotIndex];
+          data[pivotIndex] = data[minIndex];
+          data[minIndex] = h;
+
+          for (int i = 0; i < (data.Length - pivotIndex) / 2; ++i) {
+            h = data[pivotIndex + 1 + i];
+            data[pivotIndex + 1 + i] = data[data.Length - 1 - i];
+            data[data.Length - 1 - i] = h;
+          }
+        }
+      }
+      while (Enumerable.Range(0, initial.Length).Any(i => dict[initial[i]] != dict[data[i]]));
+    }
+
+    /// <summary>
+    /// Enumerate permutations starting from items
+    /// </summary>
+    /// <param name="items">initial permutation to use</param>
+    public static IEnumerable<T[]> Permutations<T>(IEnumerable<T> items) =>
+      Permutations(items, EqualityComparer<T>.Default);
 
     #endregion Public
   }
