b84157271b84cc6990bcedcb5592469bd500b0f5
Dmitry Bychenko <Zwerg.CopperNickel@gmail.com>
2022-01-19 14:13:56 +0300

    Gray Code Min Difference

--- Linq/Generators/HigherArithmetics.Linq.Generators.GrayCodes.cs
+++ Linq/Generators/HigherArithmetics.Linq.Generators.GrayCodes.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace HigherArithmetics.Linq.Generators {
 
@@ -49,6 +50,47 @@ namespace HigherArithmetics.Linq.Generators {
         }
 
         yield return result;
+      }
+    }
+
+    /// <summary>
+    /// Enumrate Gray codes; ensures mininum difference
+    /// </summary>
+    /// <param name="length">Length</param>
+    /// <param name="radix">Radix</param>
+    /// <returns></returns>
+    /// <exception cref="ArgumentOutOfRangeException">On negative length or radix </exception>
+    public static IEnumerable<int[]> EnumerateMinDufference(int length, int radix) {
+      if (length < 0)
+        throw new ArgumentOutOfRangeException(nameof(length));
+      if (radix < 0)
+        throw new ArgumentOutOfRangeException(nameof(radix));
+
+      if (0 == length || 0 == radix)
+        yield break;
+
+      int[] signs = Enumerable.Repeat(1, length).ToArray();
+      int[] current = new int[length];
+
+      for (bool keep = true; keep;) {
+        yield return current.ToArray();
+
+        keep = false;
+
+        for (int i = current.Length - 1; i >= 0; --i) {
+          int d = current[i] + signs[i];
+
+          if (d >= 0 && d < radix) {
+            current[i] = d;
+
+            for (int j = i + 1; j < signs.Length; ++j)
+              signs[j] = -signs[j];
+
+            keep = true;
+
+            break;
+          }
+        }
       }
     }
 
